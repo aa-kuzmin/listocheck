5e4df6b07df05971b5667ca4e3a324118b8418b6
Алексей Кузьмин <aa-kuzmin@mail.ru>
2026-08-04 07:53:42 +0400

    первый коммит разбиения на части

---
+++ lib/utils/errors.dart
@@ -0,0 +1,16 @@
+﻿enum AppError {errLoadSettings, errSaveSettings, errLoadList, errSaveList, }
+
+sealed class Result<T> {
+  const Result();
+}  
+
+final class Success<T> extends Result<T> {
+  final T data;
+  const Success(this.data);
+}
+
+final class Failure extends Result<void> {
+  final AppError error;
+  const Failure(this.error);
+}
+
