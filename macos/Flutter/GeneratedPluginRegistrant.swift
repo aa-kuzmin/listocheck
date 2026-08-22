fad091deec1e7d9e8a1a1307eb3bb85f74709503
Алексей Кузьмин <aa-kuzmin@mail.ru>
2026-08-22 12:17:21 +0400

    Добавил ещё отладки. Перенёс проект на другой диск. Всё падает. Попробую создать проект с нуля.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import google_sign_in_ios
+import open_file_mac
+import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
